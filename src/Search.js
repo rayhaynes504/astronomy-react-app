50556dd100c88d01a78bd56f7ad67d5abb8ff515
rayhaynes504 <twotone504@gmail.com>
2022-01-06 11:08:09 -0600

    set state on search component

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Search(props) {
+	const [specificDateValue, setSpecificDateValue] = useState('');
+	const [fromDateValue, setFromDateValue] = useState('')
+	const [toDateValue, setToDateValue] = useState('')
 
-	function grabInputValue (event) {
-		event.preventDefault()
-		console.log(event.target[0].value)
+	function grabInputValue(event) {
+		event.preventDefault();
+		if (event.target[0].value) {
+			console.log(event.target[0].value);
+		}
+		if (event.target[1].value && event.target[2].value) {
+			console.log(event.target[1].value, event.target[2].value);
+		}
+	}
+
+	function changeHandler(event) {
+		if (event.target.id === 'date-box') {
+			setSpecificDateValue(event.target.value);
+		}
+		if (event.target.id === 'from-date') {
+			setFromDateValue(event.target.value);
+		}
+		if (event.target.id === 'to-date') {
+			setToDateValue(event.target.value);
+		}
 	}
 
 	return (
 		<div className='search-container'>
 			<h1>Search For Your Images</h1>
-			<form onSubmit={grabInputValue}>
+			<form onSubmit={grabInputValue} onChange={changeHandler}>
 				<label htmlFor='date-box'>Search specific date:</label>
 				<input id='date-box' type='date' min='1995-06-16' />
 				{/* <button>Search!</button> */}
