2b8152a1978d70bf04015361fb3bf79095badcc2
rayhaynes504 <twotone504@gmail.com>
2022-01-16 20:14:06 -0600

    add about modal

--- src/About.js
+++ src/About.js
@@ -4,9 +4,18 @@ function About(props) {
 	return (
 		<div className='modal'>
 			<h4>Welcome to the Astronomy Picture Search!</h4>
-            <p>This app uses NASA's Astronomy Picture of the Day API to bring forth every picture uploaded since 1995! </p>
-            <p>Search for a detailed result by inputting a date into the top search field. </p>
-            <p>Search for a list of pictures by using the date range search field. Click on a result to view its detailed information. </p>
+			<p>
+				This app uses NASA's Astronomy Picture of the Day API to bring forth
+				every picture uploaded since 1995!
+			</p>
+			<p>
+				Search for a detailed result by inputting a date into the top search
+				field.
+			</p>
+			<p>
+				Search for a list of pictures by using the date range search field.
+				Click on a result to view its detailed information.
+			</p>
 			<button
 				className='modal-close'
 				onClick={(event) => {
